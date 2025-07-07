4159e98eea98cedda0bc02ed9ee2582af99a69df
laibinglin <1154246882@qq.com>
2025-07-07 11:03:39 +0800

    update

--- components.d.ts
+++ components.d.ts
@@ -31,6 +31,8 @@ declare module 'vue' {
     UniSteps: typeof import('./src/components/uni-steps/uni-steps.vue')['default']
     UniTransition: typeof import('./src/components/uni-transition/uni-transition.vue')['default']
     WdButton: typeof import('wot-design-uni/components/wd-button/wd-button.vue')['default']
+    WdGrid: typeof import('wot-design-uni/components/wd-grid/wd-grid.vue')['default']
+    WdGridItem: typeof import('wot-design-uni/components/wd-grid-item/wd-grid-item.vue')['default']
     WdMessageBox: typeof import('wot-design-uni/components/wd-message-box/wd-message-box.vue')['default']
     WdTag: typeof import('wot-design-uni/components/wd-tag/wd-tag.vue')['default']
     Webview: typeof import('./src/components/webview.vue')['default']
--- src/pages/index/workplace.vue
+++ src/pages/index/workplace.vue
@@ -1,7 +1,33 @@
 <script setup></script>
 
 <template>
-  <view>工作室</view>
+  <view>案件</view>
+  <wd-grid clickable :column="4" >
+    <wd-grid-item icon="picture" text="全部案件" use-icon-slot>
+      <template #icon>
+        <i class="iconfont icon-case-file"></i>
+      </template>
+    </wd-grid-item>
+  </wd-grid>
+  <view>团队</view>
+  <wd-grid clickable :column="4">
+    <wd-grid-item icon="picture" text="文字" />
+    <wd-grid-item icon="picture" text="文字" />
+    <wd-grid-item icon="picture" text="文字" />
+  </wd-grid>
+  <view>政策</view>
+  <wd-grid clickable :column="4">
+    <wd-grid-item icon="picture" text="文字" />
+    <wd-grid-item icon="picture" text="文字" />
+    <wd-grid-item icon="picture" text="文字" />
+  </wd-grid>
+  <view>文章</view>
+  <wd-grid clickable>
+    <wd-grid-item icon="picture" text="文字" />
+    <wd-grid-item icon="picture" text="文字" />
+    <wd-grid-item icon="picture" text="文字" />
+    <wd-grid-item icon="picture" text="文字" />
+  </wd-grid>
 </template>
 
 <style scoped></style>
