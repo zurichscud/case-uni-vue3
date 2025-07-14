fac2ae6db8b31ce74877a0322968dbfad7b8634d
laibinglin <1154246882@qq.com>
2025-07-14 16:13:34 +0800

    update

--- src/components/YpScrollView.vue
+++ src/components/YpScrollView.vue
@@ -1,10 +1,12 @@
 <script setup>
+
 const refreshing = ref(false)
 const moreStatus = ref('more')
 const pageParams = ref({
   pageNum: 1,
   pageSize: 4,
 })
+
 const props = defineProps({
   query: {
     type: Function,
@@ -12,11 +14,42 @@ const props = defineProps({
   },
 })
 
+async function getData() {
+  try {
+
+    const { rows, total } = await props.query(pageParams.value)
+
+    if (pageParams.value.pageNum === 1) {
+      caseList.value = rows
+    } else {
+      caseList.value.push(...rows)
+    }
+
+    if (caseList.value.length < total) {
+      moreStatus.value = 'more'
+      pageParams.value.pageNum++
+    } else {
+      moreStatus.value = 'noMore'
+    }
+
+  } catch (error) {
+    console.error('加载案件列表失败:', error)
+    uni.showToast({
+      title: '加载失败，请重试',
+      icon: 'error',
+    })
+  } finally {
+    loading.value = false
+    loadingMore.value = false
+    refreshing.value = false
+  }
+}
+
  async function handleRefresh() {
   refreshing.value = true
   pageParams.value.pageNum = 1
   moreStatus.value = 'more'
-  await props.query()
+  await getData()
   refreshing.value = false
 }
 
@@ -24,7 +57,7 @@ function handleScrolltoLower() {
   if (moreStatus.value === 'noMore') {
     return
   }
-  props.query()
+  getData()
 }
 </script>
 
